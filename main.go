a95d5a8a9906604514b40ac4832ee32a72e3c21d
Tandokukou <denisencko.kiril@yandex.ru>
2024-10-05 14:34:37 +0300

    1

--- main.go
+++ main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
+var message string
+
+type requestBody struct {
+	Message string `json:"message"`
+}
+
+func messageHandler(w http.ResponseWriter, r *http.Request) {
+	var req requestBody
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Oops", http.StatusBadRequest)
+		return
+	}
+	message = req.Message
+}
+
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+	fmt.Fprintln(w, "hello, "+message)
 }
 
 func main() {
 	router := mux.NewRouter()
-	// наше приложение будет слушать запросы на localhost:8080/api/hello
+	router.HandleFunc("/api/message", messageHandler).Methods("POST")
 	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
 	http.ListenAndServe(":8080", router)
 }
